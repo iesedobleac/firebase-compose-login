63e2e07b9b71c3022160b17db32df6deb2993f6f
Isaac <isaacdelosredi@gmail.com>
2021-10-24 17:12:06 +0200

    Se añade a la aplicación la navegación del apartado de registro y viceversa.

--- app/src/main/java/com/isaacdelosreyes/firebaselogincompose/components/RegisterInput.kt
+++ app/src/main/java/com/isaacdelosreyes/firebaselogincompose/components/RegisterInput.kt
@@ -1,11 +1,9 @@
 package com.isaacdelosreyes.firebaselogincompose.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -15,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -25,7 +22,7 @@ import androidx.compose.ui.unit.sp
 import com.isaacdelosreyes.firebaselogincompose.ui.theme.White900
 
 @Composable
-fun LoginInput(textLabel: String, imageVector: ImageVector, isTypePassword: Boolean = false) {
+fun RegisterInput(textLabel: String, isTypePassword: Boolean = false) {
     val valueLabel = remember { mutableStateOf("") }
 
     TextField(
@@ -42,13 +39,6 @@ fun LoginInput(textLabel: String, imageVector: ImageVector, isTypePassword: Bool
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent
         ),
-        leadingIcon = {
-            Icon(
-                imageVector = imageVector,
-                contentDescription = "Icono del campo de texto",
-                tint = Color.Gray
-            )
-        },
         visualTransformation = if (isTypePassword) {
             PasswordVisualTransformation()
 
